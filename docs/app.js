4de4f7a626a042264aa610cf52308ae08f3f2cfc
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2023-09-08 17:17:48 -0400

    static map of virginia; slow, very slow

--- docs/app.js
+++ docs/app.js
@@ -1,14 +1,16 @@
 // Open a connection to IndexedDB
-const dbName = "tigerDB";
+const dbName = "verygoodname";
 const dbVersion = 1;
+const initView = ["51"];
 
-const openDB = () => {
+const openDB = (storeName) => {
     return new Promise((resolve, reject) => {
-        const request = indexedDB.open(dbName, dbVersion);
+        const request = window.indexedDB.open(storeName, dbVersion);
 
         request.onupgradeneeded = (event) => {
             const db = event.target.result;
-            db.createObjectStore("tigerShapes", { keyPath: "id" });
+            const objectStore = db.createObjectStore(storeName, { keyPath: "id" });
+            objectStore.createIndex("data", "data", { unique: false });
         };
 
         request.onsuccess = (event) => {
@@ -37,28 +39,37 @@ async function fetchData(url) {
     }
   }
 
-const storeData = (id, data) => {
-    console.log("Storing data: " + data);
-    console.log(JSON.stringify(data, null, 4)),
+const storeData = (storeName, id, data) => {
+    console.log("Storing data: ");
+    // console.log(JSON.stringify(data, null, 4)),
     // Save the data into IndexedDB
-    openDB().then(db => {
-        const transaction = db.transaction(["tigerShapes"], "readwrite");
-        const store = transaction.objectStore("tigerShapes");
-        request = store.put({ id: id, data: data});
+    openDB(storeName).then(db => {
+        //const transaction = db.transaction([storeName], "readwrite");
+
+        // Store values in the newly created objectStore.
+        const objectStore = db
+          .transaction(storeName, "readwrite")
+          .objectStore(storeName);
+
+        const request = objectStore.add({'id': id, 'data': data});
+
         request.onsuccess = function (evt) {
             // do something after the add succeeded
             return true;
         };
+
+        // const store = transaction.objectStore(storeName);
+        // const request = store.put({ 'id': id, 'data': data});
+
     })    
 };
 
-
 // Render the map using D3.js and the fetched data
-const renderMap = (id, stored) => {
+const renderMap = (storeName, id) => {
     console.log("Rendering " + id);
-    openDB().then(db => {
-        const transaction = db.transaction(["tigerShapes"], "readonly");
-        const store = transaction.objectStore("tigerShapes");
+    openDB(storeName).then(db => {
+        const transaction = db.transaction(storeName, "readonly");
+        const store = transaction.objectStore(storeName);
         const request = store.openCursor(id);
         
         request.onsuccess = (event) => {
@@ -76,19 +87,49 @@ const renderMap = (id, stored) => {
                     // // This part depends on the specific structure of your GeoJSON data
                     // // For example, if you have polygons:
                     // console.log(JSON.stringify(value, null, 4));
-                    console.log(value.data["features"][0]["geometry"]["coordinates"]);
-                    d3.select("#map").append("svg")
-                        .selectAll("Polygon")
-                        .data(value.data["features"])
-                        .enter().append("path")
-                        .attr("d", d3.geoPath())
+                    // console.log(value.data["features"][0]["geometry"]["coordinates"]);
+                    // console.log(value.data)
+
+                    let width = 900,
+                        height = 700;
+
+                    let projection = d3.geoMercator()
+                        //.fitSize([width, height], value.data)
+                        .scale(5000)
+                        .translate([width / 2, height / 2])
+                        .center([-78, 37]);
+                    let geoGenerator = d3.geoPath().projection(projection);
+
+                    if (d3.select('#map-svg').size() === 0) {
+                        let svg = d3.select("#map").append("svg").attr("id", "map-svg").attr("preserveAspectRatio", "xMinYMin meet")
+                            .attr("viewBox", "0 0 " + width + " " + height).style("background","#c9e8fd")
+                            .classed("svg-content", true);
+                    }
+
+                    d3.select("#map-svg")//.append("svg")
+                        //.attr("width", "95%")
+                        //.attr("height", "95%")
+                        //.style("padding", "2.5%")
+                        .selectAll("npath")
+                        .data(value.data.features)
+                        //.enter()
+                        .join("path")
+                        .attr("d", geoGenerator)
                         .style("fill", "steelblue")
-                        .style("stroke", "white");      
+                        .style("stroke", "white")
+                        .on("click", handleClick);
+    
                 }else{
                     console.log("Key is undefined");
                 }
                 cursor.continue();
             }
+
+            // for testing
+            function handleClick(event, d, i) {
+                d3.select("#summary-table").append("text").text(d.properties.ALAND20)
+            }
+
         };
 
         request.onerror = (event) => {
@@ -99,34 +140,77 @@ const renderMap = (id, stored) => {
     // projection.fitExtent([[100,100],[w-100,h-100]],map);
 };
 
+
 // Main function to initialize the app
 const initApp = () => {
-    // Check if the data is already in IndexedDB or fetch it if not
-    openDB().then(db => {
-        const transaction = db.transaction(["tigerShapes"], "readonly");
-        const store = transaction.objectStore("tigerShapes");
-        const request = store.get("010010201001");
+    openDB(dbName).then(db => {
+        // clean this up...
+        fetchData('https://raw.githubusercontent.com/wanghalan/shapes/main/manifest.json')
+            .then((data) => {
+                const transaction = db.transaction([dbName], "readonly");
+                const store = transaction.objectStore(dbName);
 
-        request.onsuccess = (event) => {
-            if (event.target.result) {
-                // Data is already in IndexedDB, render the map directly
-                console.log("Data is already in IndexedDB, render the map directly");
-                renderMap("010010201001");
-            } else {
-                // Data is not in IndexedDB, fetch it and then render the map
-                console.log("Fetching data...");
-                const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/data/01001/010010201001.geojson';
-                fetchData(url)
-                .then(data => storeData("010010201001", data))
-                .then(renderMap)
-                .catch(error => {
-                  // Handle errors if needed
-                  console.error('Error in the asynchronous process:', error);
-                });
-                
-            }
-        };
+                data.forEach((filename, index) => {
+                    let bgCode = filename.split("/").pop().slice(0, 12)
+
+                    // if not in initial view
+                    if (!initView.includes(bgCode.slice(0, 2))) {
+                        return;
+                    }
+
+                    const request = store.get(bgCode);
+                    store.get(bgCode).onsuccess = function(event){
+                        if (event.target.result) {
+                            // Data is already in IndexedDB, render the map directly
+                            console.log("Data is already in IndexedDB, render the map directly");
+                            renderMap(dbName, bgCode);
+                        } else {
+                            // Data is not in IndexedDB, fetch it and then render the map
+                            console.log("Fetching data...");
+                            const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/' + filename;
+
+                            fetchData(url)
+                                .then(geos => storeData(dbName, bgCode, geos))
+                                .then(geos => renderMap(dbName, bgCode))
+                                .catch(error => {
+                                  // Handle errors if needed
+                                  console.error('Error in the asynchronous process:', error);
+                                });        
+                        }
+                    };
+                })
+            }).catch(error => {
+                console.error('Error in the asynchronous process:', error);
+            });
     });
+
+    // Check if the data is already in IndexedDB or fetch it if not
+    // openDB(dbName).then(db => {
+    //     const transaction = db.transaction([dbName], "readonly");
+    //     const store = transaction.objectStore(dbName);
+     
+    //     const request = store.get("010010201001");
+
+    //     request.onsuccess = (event) => {
+    //         if (event.target.result) {
+    //             // Data is already in IndexedDB, render the map directly
+    //             console.log("Data is already in IndexedDB, render the map directly");
+    //             renderMap(dbName, "010010201001");
+    //         } else {
+    //             // Data is not in IndexedDB, fetch it and then render the map
+    //             console.log("Fetching data...");
+    //             const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/data/01001/010010201001.geojson';
+    //             fetchData(url)
+    //             .then(data => storeData(dbName, "010010201001", data))
+    //             .then(data => renderMap(dbName, "010010201001", data))
+    //             .catch(error => {
+    //               // Handle errors if needed
+    //               console.error('Error in the asynchronous process:', error);
+    //             });
+                
+    //         }
+    //     };
+    // });
 };
 
 // Initialize the app
