5a5b70b15ede242f1218f7eb96a0df227c642083
alan <ahw9f@virginia.edu>
2023-09-14 17:00:01 -0400

    saving work

--- docs/load_data.js
+++ docs/load_data.js
@@ -4,14 +4,40 @@ const dbVersion = 1;
 const refStates = ["51"];
 const shapeManifestUrl = 'https://raw.githubusercontent.com/wanghalan/shapes/main/manifest.json';
 
+
+Array.prototype.geoidMatch= function(geoid){
+    let ref = ""+ geoid.slice(0, 2);
+    return refStates.includes(ref);
+}
+
+/**
+ * Wait to get data from the url, returning the javascript content on success, and throwing an error on fail
+ * @param {string} url 
+ * @returns {json} jsonData
+ */
+async function fetchJSON(url) {
+    try {
+      const response = await fetch(url); // Make the asynchronous request
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const jsonData = await response.json(); // Parse JSON data
+      return jsonData;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+}
+
+
 const openDB = (storeName) => {
     return new Promise((resolve, reject) => {
         const request = window.indexedDB.open(storeName, dbVersion);
 
         request.onupgradeneeded = (event) => {
             const db = event.target.result;
-            const objectStore = db.createObjectStore(storeName, { keyPath: "id" });
-            objectStore.createIndex("data", "data", { unique: false });
+            const objectStore = db.createObjectStore(storeName, { keyPath: "key" });
+            objectStore.createIndex("value", "value", { unique: false });
         };
 
         request.onsuccess = (event) => {
@@ -25,126 +51,108 @@ const openDB = (storeName) => {
     });
 };
 
-
 /**
  * 
- * @param {String} url 
- * @returns {String}
+ * @param {string} storeName 
+ * @param {string} key 
+ * @param {string} value 
  */
-async function fetchData(url) {
-    try {
-      const response = await fetch(url); // Make the asynchronous request
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const jsonData = await response.json(); // Parse JSON data
-      return jsonData;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
-    }
-  }
-
-const storeData = (storeName, id, data) => {
-    console.log("Storing data: ");
-    // console.log(JSON.stringify(data, null, 4)),
+const storeData = (storeName, key, value) => {
     // Save the data into IndexedDB
     openDB(storeName).then(db => {
-        //const transaction = db.transaction([storeName], "readwrite");
-
         // Store values in the newly created objectStore.
         const objectStore = db
           .transaction(storeName, "readwrite")
           .objectStore(storeName);
-
-        const request = objectStore.add({'id': id, 'data': data});
-
+        const request = objectStore.add({'key': key, 'value': value});
         request.onsuccess = function (evt) {
             // do something after the add succeeded
             return true;
         };
-
-        // const store = transaction.objectStore(storeName);
-        // const request = store.put({ 'id': id, 'data': data});
-
     })    
 };
 
-const syncShapefiles = (storeName) => {
-    // if works... move to own fn
-    openDB(storeName).then(db => {
-        fetchData(shapeManifestUrl)
-        .then((data) => {
-            const transaction = db.transaction([dbName], "readonly");
-            const store = transaction.objectStore(dbName);
+function syncShapefiles (storeName)  {
+    const promises = []
+    openDB(storeName).then(db => { //Once the database is opened
+        fetchJSON(shapeManifestUrl).then(filepaths => { //And once the JSON is retrieved
+            for (i = 0; i< filepaths.length; i++){
 
-            data.forEach((filename, index) => {
-                let bgCode = filename.split("/").pop().slice(0, 12)
+                let county = filepaths[i].split("/").pop().slice(0, 5);
 
-                const request = store.get(bgCode);
-                store.get(bgCode).onsuccess = function(event){
-                    if (!event.target.result)  {
-                        // Data is not in IndexedDB, fetch it and then render the map
-                        console.log("Fetching data...");
-                        const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/' + filename;
+                if (!refStates.geoidMatch(county)){ // if no matches
+                    continue;
+                }
 
-                        fetchData(url)
-                            .then(geos => storeData(dbName, bgCode, geos))
-                            //.then(geos => renderMap(dbName, bgCode))
-                            .catch(error => {
-                                // Handle errors if needed
-                                console.error('Error in the asynchronous process:', error);
-                            });        
-                    }
-                };
-            })
-        }).catch(error => {
-            console.error('Error in the asynchronous process:', error);
-        });
-    });
-}
+                let filepath = filepaths[i];
+                const transaction = db.transaction([storeName], "readonly");
+                const store = transaction.objectStore(storeName);                
+                const req = store.openCursor(county);
 
-const syncRepoData = (storeName) => {
-    // if works... move to own fn
-    openDB(storeName).then(db => {
-        fetchData(shapeManifestUrl)
-        .then((data) => {
-            const transaction = db.transaction([dbName], "readonly");
-            const store = transaction.objectStore(dbName);
-
-            data.forEach((filename, index) => {
-                let measure = "blah"
-                const request = store.get(measure);
-                store.get(bgCode).onsuccess = function(event){
-                    if (!event.target.result)  {
-                        // Data is not in IndexedDB, fetch it and then render the map
-                        const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/' + filename;
+                req.onsuccess= function(e){
+                    var cursor = e.target.result;
+                    if (!cursor){
+                        const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/' + filepath;
                         console.log(url);
-                        // fetchData(url)
-                        //     .then(geos => storeData(dbName, bgCode, geos))
+                        // fetchJSON(url)
+                        //     .then(geos => storeData(storeName, county, geos))
                         //     .catch(error => {
                         //         // Handle errors if needed
                         //         console.error('Error in the asynchronous process:', error);
-                        //     });        
+                        //     });      
                     }
-                };
-            })
-        }).catch(error => {
-            console.error('Error in synchronizing repository data:', error);
+                }
+            }
         });
     });
+    return Promise.allSettled(promises);
 }
+    
+        
 
 
+// const syncRepoData = (storeName) => {
+//     // if works... move to own fn
+//     openDB(storeName).then(db => {
+//         fetchData(shapeManifestUrl)
+//         .then((data) => {
+//             const transaction = db.transaction([dbName], "readonly");
+//             const store = transaction.objectStore(dbName);
 
+//             data.forEach((filename, index) => {
+//                 let measure = "blah"
+//                 const request = store.get(measure);
+//                 store.get(bgCode).onsuccess = function(event){
+//                     if (!event.target.result)  {
+//                         // Data is not in IndexedDB, fetch it and then render the map
+//                         const url = 'https://raw.githubusercontent.com/wanghalan/shapes/main/' + filename;
+//                         console.log(url);
+//                         // fetchData(url)
+//                         //     .then(geos => storeData(dbName, bgCode, geos))
+//                         //     .catch(error => {
+//                         //         // Handle errors if needed
+//                         //         console.error('Error in the asynchronous process:', error);
+//                         //     });        
+//                     }
+//                 };
+//             })
+//         }).catch(error => {
+//             console.error('Error in synchronizing repository data:', error);
+//         });
+//     });
+// }
 
 // Main function to initialize the app
-const syncAllData = () => {
-    syncShapefiles("shapefiles");
-    syncRepoData("testing"); //this can be the name of the repository
+function syncAllData() {
 
 }
 
 
-// Synchronize the data
-syncAllData();
+
+const start = Date.now();
+syncShapefiles("shapefiles").then(()=>{
+    const end = Date.now();
+    console.log('Execution time: '+ (end - start) + ' ms');
+})
+
+
